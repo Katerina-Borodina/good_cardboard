486cc6333bf413bcdd783010a3a5a464283a5bd6
MissKiss <borodina1902@icloud.com>
2018-10-23 19:27:26 +0300

    add new block

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,81 @@ $(document).ready(function() {
         $('.overlay').hide();
     });
 });
+
+$(document).ready(function(){
+    $('.single-item').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: 'unslick'
+            },
+            {
+                breakpoint: 992,
+                settings: 'unslick'
+            }
+        ],
+        prevArrow: '<div class="slider-arrow_main slider-arrow_main__left"></div>',
+        nextArrow: '<div class="slider-arrow_main slider-arrow_main__right"></div>'
+    });
+
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        infinite: false,
+        dots: false,
+        centerMode: false,
+        focusOnSelect: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ],
+        prevArrow: '<div class="slider-arrow slider-arrow_left"></div>',
+        nextArrow: '<div class="slider-arrow slider-arrow_right"></div>'
+    });
+    $('.multiple-items').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            }
+        ],
+        prevArrow: '<div class="slider-arrow_main slider-arrow_main__left"></div>',
+        nextArrow: '<div class="slider-arrow_main slider-arrow_main__right"></div>'
+    });
+});
