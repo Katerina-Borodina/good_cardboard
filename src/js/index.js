8e653c0f1db9ed02f9bc34b44049af66a0681701
MissKiss <borodina1902@icloud.com>
2019-03-15 01:46:54 +0200

    +
    +

--- src/js/index.js
+++ src/js/index.js
@@ -84,3 +84,17 @@ $(document).ready(function(){
         nextArrow: '<div class="slider-arrow_main slider-arrow_main__right"></div>'
     });
 });
+
+$(function() {
+    var link = $('.m-menu-link');
+    var close = $('.close-menu');
+    var menu = $('.m-menu');
+    link.on('click', function(event) {
+        event.preventDefault();
+        menu.toggleClass('m-menu_activ');
+    });
+    close.on('click', function(event) {
+        event.preventDefault();
+        menu.toggleClass('m-menu_activ');
+    });
+});
